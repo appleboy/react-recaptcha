6cfc96aa7d7059ee74a627751f3bef2ab0307f66
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-12-14 00:24:30 +0800

    initial render function.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 'use strict';
 
 var React = require('react');
+
+var Recaptcha = React.createClass({
+  render: function() {
+    <div class="g-recaptcha" data-sitekey="this.props.siteKey"></div>
+  }
+});
+
+module.exports = Recaptcha;
